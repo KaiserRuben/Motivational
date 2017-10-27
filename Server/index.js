27742ffcda90cc9d308c95d0a868d1539198f3e9
Debian Live user <user@localhost.localdomain>
2017-10-27 17:01:31 +0000

    Added (POST) and worked on (PUT), commented half of code for undestanding

--- Server/index.js
+++ Server/index.js
@@ -14,11 +14,32 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 */
+
+// Server Port :)
 const port = 3000
 
-const express = require('express')
-const app = express()
+// import modules express and body parser
+const express = require('express');
+const bodyparser = require('body-parser');
 
+// creates constant app
+const app = express();
+
+// use constant bodyparser (in json) in  express app
+app.use(bodyparser.json());
+
+// create class user and sets parameters using constructor
+class User {
+  constructor(data) {
+    this.name = data.name;
+    this.email = data.email;
+    this.password = data.password;
+    this.profileImg = data.profileImg;
+    // Random Id generator
+    this.id = Math.random();
+  }
+}
+// User Array, saved in json
 const users = [
   {
     id: "47dzw6d",
@@ -43,7 +64,9 @@ const users = [
   }
 ];
 
+// gives user data back (json formaf)
 app.get('/user', function (req, res) {
+  // sends response in json format
   res.json(users.map( ({ id, name, email, profileImg }) => {
     return {
       id,
@@ -53,12 +76,13 @@ app.get('/user', function (req, res) {
     }
   }));
 })
-
+// gets back specific user by id
 app.get('/user/:id', (req, res) => {
-
+  // transfers :id to constant userId
   const userId = req.params.id;
+  // find user by user id in user array
   const foundUser = users.find(user => user.id === userId);
-
+  // if a user has been found -> gives back user not psw
   if (foundUser) {
     res.json({
       id: foundUser.id,
@@ -67,17 +91,38 @@ app.get('/user/:id', (req, res) => {
       profileImg: foundUser.profileImg
     });
   } else {
+    // else send "Not found"
     res.send(404);
   }
 
 });
 
+
+
+// new user
 app.post('/user', function (req, res) {
-  res.send('Got a POST request at /user')
+  console.log(req.body)
+  // -- res.send(req.body)
+  // if something in requests body
+  if (req.body) {
+    const tmpUsr = new User(req.body)
+    users.push(tmpUsr)
+    res.json(tmpUsr)
+  } else {
+    res.send(400) // bad request
+  }
 })
 
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
+app.put('/user/:id', function (req, res) {
+  console.log(":: PUT User")
+  const userId = req.params.idea
+  const userObj = req.body
+  if (userObj) {
+    const foundUser = users.find(user => user.id === userId)(
+    if (foundUser) {
+
+    }
+  }
 })
 
 app.delete('/user/:id', function (req, res) {
